e21537aae4ac38886e74165048ed8de4142862b7
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-06-22 17:29:15 +0100

    feat: root file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppNavigationContainer from "./src/navigation/AppNavigationContainer";
+import { Provider } from "react-redux";
+import { store } from "@store/main";
+import { initInterceptors } from "@utils/axios-utils/axiosInterceptors";
+
+
+// init axios interceptor
+initInterceptors();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <AppNavigationContainer />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
